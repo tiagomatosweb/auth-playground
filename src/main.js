f7e851a496795d41e09181765beb05f75919e025
Tiago Matos <tiago@tiagomatos.com>
2026-02-16 15:54:39 +1100

    Add vue-route and nuxtui

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import ui from '@nuxt/ui/vue-plugin'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+app.use(router)
+app.use(ui)
+
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ui from '@nuxt/ui/vite'
+import VueRouter from 'unplugin-vue-router/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    VueRouter(),
+    vue(), 
+    ui()
+  ],
 })
